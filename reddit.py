945368faba556528f5f17d31250424f2d652ee1d
Firefox0 <meow@meow.meow>
2020-02-08 15:17:17 +0100

    minor name changes

--- reddit.py
+++ reddit.py
@@ -49,33 +49,32 @@ class RedditApp:
         self.limit_textctrl = wx.TextCtrl(panel, pos=(100, 45))
 
         directory_button = wx.Button(panel, label="Select", pos=(100, 75))
-        directory_button.Bind(wx.EVT_BUTTON, self.set_directory)
+        directory_button.Bind(wx.EVT_BUTTON, self._set_directory)
         scrape_button  = wx.Button(panel, label="Scrape", pos=(100, 105))
-        scrape_button.Bind(wx.EVT_BUTTON, self.set_scrape)
+        scrape_button.Bind(wx.EVT_BUTTON, self._set_scrape)
 
         self.progress_bar = wx.Gauge(panel, range=100, pos=(20, 160), size=(265, 15))
         self.directory = os.getcwd()
 
-        self.scrape_thread = threading.Thread(target=self.scrape, daemon=True)
+        self.scrape_thread = threading.Thread(target=self._scrape, daemon=True)
 
         self.frame.Show()
+
+    def _download_media(self, urls, update_progress=True):
+        # self._check_directory()
+        urls = self._check_duplicates(urls)
+        responses = self._get_responses(urls)
+        self._download(responses)
+        self._reset()
+        self._message_box("Download completed")
+        return 1
 
     def _get_submissions(self, subreddit_name, limit):
         submissions = set()
         for submission in self.reddit.subreddit(subreddit_name).stream.submissions():
             submissions.add(submission)
             if len(submissions) == limit: 
-                return submissions
-
-    def _get_media(self, subreddit_name, limit, extensions):
-        media = set()
-        for submission in self.reddit.subreddit(subreddit_name).stream.submissions():
-            url = submission.url
-            if not any(e in url for e in self.media_hosts):
-                if any(ext in url for ext in extensions):
-                    media.add(url)
-                    if len(media) == limit:
-                        return media 
+                return submissions 
 
     def _psaw_get_media(self, filter, subreddit="pics", limit=0, after=0):
         submissions = self._psaw_search_submissions("url", subreddit, limit)
@@ -104,7 +103,7 @@ class RedditApp:
     def _check_duplicates(self, urls):
         return [url for url in urls if not os.path.exists(f"{self.directory}\\{url}")]
     
-    def get_media(self, filter, subreddit_name, limit): 
+    def _get_media(self, filter, subreddit_name, limit): 
         urls = self._psaw_get_media(filter, subreddit=subreddit_name, limit=limit)
         return self._fix_media_urls(urls)
 
@@ -117,7 +116,7 @@ class RedditApp:
                     for chunk in r:
                         f.write(chunk)
             if update_progress:
-                self.update_progress(r.url)
+                self._update_progress(r.url)
         return 1
 
     def _check_directory(self):
@@ -128,47 +127,38 @@ class RedditApp:
         requests = (grequests.get(u) for u in urls)
         return grequests.imap(requests)
 
-    def download_media(self, urls, update_progress=True):
-        # self._check_directory()
-        urls = self._check_duplicates(urls)
-        responses = self._get_responses(urls)
-        self._download(responses)
-        self.reset()
-        self.message_box("Download completed")
-        return 1
-
     def _psaw_search_submissions(self, filter, subreddit="pics", limit=0, after=0):
         return self.PS.search_submissions(filter=filter, subreddit=subreddit, limit=limit, after=after)
 
-    def set_directory(self, event): 
+    def _set_directory(self, event): 
         directory_dialog = wx.DirDialog(self.frame, "Choose a directory:", defaultPath=self.directory)
         if directory_dialog.ShowModal() == wx.ID_OK: 
             self.directory = directory_dialog.GetPath()
 
-    def update_progress(self, media_name): 
+    def _update_progress(self, media_name): 
         self.download_text.SetLabel(f"Downloading: {media_name}")
         self.progress_text.SetLabel(f"{self.count}/{self.progress_bar_range}")
         self.count += 1
         self.progress_bar.SetValue(self.count)
 
-    def set_progress_range(self, progress_range):
+    def _set_progress_range(self, progress_range):
         self.progress_bar_range = progress_range
         self.progress_bar.SetRange(progress_range)
 
-    def set_scrape(self, event): 
+    def _set_scrape(self, event): 
         self.subreddit = self.subreddit_textctrl.GetValue()
         self.limit = self.limit_textctrl.GetValue()
         if self.subreddit and self.limit: 
             self.limit = int(self.limit)
         else: 
-            self.message_box("Subreddit or Limit missing")
+            self._message_box("Subreddit or Limit missing")
         
         self.scrape_thread.start()
 
-    def message_box(self, message):
+    def _message_box(self, message):
         wx.MessageBox(message, "Info", wx.OK)
 
-    def reset(self): 
+    def _reset(self): 
         self.subreddit = None 
         self.subreddit_textctrl.SetValue("")
         self.limit = None 
@@ -179,10 +169,20 @@ class RedditApp:
         self.download_text.SetLabel("Waiting...")
         self.progress_text.SetLabel("0/0")
     
-    def scrape(self): 
-        urls = self.get_media("url", self.subreddit, self.limit)
-        self.set_progress_range(len(urls))
-        self.download_media(urls) 
+    def _scrape(self): 
+        urls = self._get_media("url", self.subreddit, self.limit)
+        self._set_progress_range(len(urls))
+        self._download_media(urls) 
+
+    def __get_media(self, subreddit_name, limit, extensions):
+        media = set()
+        for submission in self.reddit.subreddit(subreddit_name).stream.submissions():
+            url = submission.url
+            if not any(e in url for e in self.media_hosts):
+                if any(ext in url for ext in extensions):
+                    media.add(url)
+                    if len(media) == limit:
+                        return media
 
 if __name__ == "__main__":
     app = wx.App()
